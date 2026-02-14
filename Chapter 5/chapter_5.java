3b9abad0f48cee618e6382f0e0bbc0870c444440
SWAPNIL VERMA <swapnilverma109@gmail.com>
2026-02-14 10:43:33 +0530

    update chapter 5

---
+++ Chapter 5/chapter_5.java
@@ -0,0 +1,98 @@
+
+    import java.util.Scanner;
+    public class chapter_5 {
+        public static void main(String[] args) {
+//        int i = 0;
+//        while(i <= 10){
+//            System.out.println(i);
+//            i++;
+
+//        int b = 10;
+//        do {
+//            System.out.println(b);
+//            b++;
+//        }while (b<5);
+
+
+            // problem no 1
+//       int i=99;
+//       while (i<=199){
+//           i++;
+//           System.out.println(i);
+//       }
+
+            //    problem 2
+
+//        System.out.println("Enter the number : ");
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        int i= 1;
+//        do {
+//            System.out.println(i);
+//            i++;
+//        }while (i<=a);
+
+//        for(int i= 1; i<=10; i++) {
+//            System.out.println(i);
+//        }
+
+            //   problem 3
+//        System.out.println("Enter Number : ");
+//        Scanner sc = new Scanner(System.in);
+//                int n = sc.nextInt();
+//
+//        for (int i=0; i<n; i++){
+//            System.out.println(2*i+1);
+//        }
+
+
+//       problem 4
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//
+//        for (int i = a; i!=0; i--){
+//            System.out.println(i);
+//        }
+//
+
+
+            //      Break and continue using loops:
+
+//        for(int i=0; i<5; i++){
+//            System.out.println(i);
+//            if(i==2){
+//                System.out.println("Ending the loop");
+//                break;
+//        }
+//        System.out.println("java is great");
+//        }
+
+//        int i=0;
+//        do{
+//            System.out.println(i);
+//            System.out.println("java is great");
+//            if(i==2){
+//                System.out.println("Ending the task");
+//                break;
+//            }
+//            i++;
+//        } while (i<5);
+//        System.out.println("Loop ends here");
+
+            //continue rule
+
+//        for(int i=0; i<5; i++) {
+//            System.out.println(i);
+//            if (i == 2) {
+//                System.out.println("Ending the loop");
+//                continue;
+//
+//            }
+//            System.out.println("java is great");
+//
+//        }
+
+
+
+        }
+    }
